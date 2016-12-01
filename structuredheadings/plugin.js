c8a940c800caebbb572dbbbbe90a3f1983be6ed0
Alex Tsai <caffodian@gmail.com>
2016-12-01 11:15:09 -0800

    more fun

--- structuredheadings/plugin.js
+++ structuredheadings/plugin.js
@@ -1,7 +1,8 @@
 (function () {
   CKEDITOR.plugins.add("structuredheadings", {
     init: function (editor) {
-      bender.assert(editor);
+      // actually, no return is required, but this shuts eslint up
+      return editor;
     }
   });
 })();
