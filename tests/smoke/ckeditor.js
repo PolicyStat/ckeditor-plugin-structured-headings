ee9b1e59597ef4ed672712ba8f0729a94d417b2f
BDeveau <brad.deveau@policystat.com>
2016-12-06 12:41:01 -0500

    Cleaning out the Lint

--- tests/smoke/ckeditor.js
+++ tests/smoke/ckeditor.js
@@ -5,59 +5,68 @@
 (function () {
 
   bender.editor = {
-    allowedForTests: 'h1; p'
+    allowedForTests: "h1; p"
   };
-  
+
   bender.test({
     setUp: function () {
       //Anything to be run before each test if needed
     },
 
     "CKEditor Loads": function () {
-      CKEDITOR.replace('editor1')
+      CKEDITOR.replace("editor1");
       assert.isObject(CKEDITOR.instances.editor1, "editor instance not found");
     },
 
     "Structured Headings Plugin Available": function () {
       assert.isObject(CKEDITOR.plugins.get("structuredheadings"), "plugin not available");
     },
-    
-    'Command Active but Off Inside Header Tag': function() {
-        this.editorBot.setHtmlWithSelection('<h1>Heading</h1>');
-        var testCommand = this.editor.getCommand( 'autoNumberHeading' );
-        
+
+    "Command Active but Off Inside Header Tag": function () {
+      this.editorBot.setHtmlWithSelection("<h1>Heading</h1>");
+      var testCommand = this.editor.getCommand("autoNumberHeading");
+
         // This is the only way I've found to position the cursor in the element
-        var range = this.editor.createRange();
-		range.moveToElementEditablePosition( this.editor.editable(), true );
-		this.editor.getSelection().selectRanges( [ range ] );
-        
-        assert.areSame( CKEDITOR.TRISTATE_OFF, testCommand.state, 'autoNumberHeading OFF in H1' );
+      var range = this.editor.createRange();
+      range.moveToElementEditablePosition(this.editor.editable(), true);
+      this.editor.getSelection().selectRanges([ range ]);
+
+      assert.areSame(
+        CKEDITOR.TRISTATE_OFF, testCommand.state,
+        "autoNumberHeading OFF in H1"
+      );
 
     },
-    
-    'Command Active and On Inside AutoNumbered Header': function() {
-        this.editorBot.setHtmlWithSelection('<h1 class="autonumber">Heading</h1>');
-        var testCommand = this.editor.getCommand( 'autoNumberHeading' );
-        
+
+    "Command Active and On Inside AutoNumbered Header": function () {
+      this.editorBot.setHtmlWithSelection("<h1 class=\"autonumber\">Heading</h1>");
+      var testCommand = this.editor.getCommand("autoNumberHeading");
+
         // This is the only way I've found to position the cursor in the element
-        var range = this.editor.createRange();
-        range.moveToElementEditablePosition( this.editor.editable(), true );
-        this.editor.getSelection().selectRanges( [ range ] );
-        
-        assert.areSame( CKEDITOR.TRISTATE_ON, testCommand.state, 'autoNumberHeading ON in H1' );
+      var range = this.editor.createRange();
+      range.moveToElementEditablePosition(this.editor.editable(), true);
+      this.editor.getSelection().selectRanges([ range ]);
+
+      assert.areSame(
+        CKEDITOR.TRISTATE_ON, testCommand.state,
+        "autoNumberHeading ON in H1"
+      );
 
     },
-    
-    'Command Disabled Outside Header': function() {
-        this.editorBot.setHtmlWithSelection('<p> Paragraph </p>');
-        var testCommand = this.editor.getCommand( 'autoNumberHeading' );
-        
+
+    "Command Disabled Outside Header": function () {
+      this.editorBot.setHtmlWithSelection("<p> Paragraph </p>");
+      var testCommand = this.editor.getCommand("autoNumberHeading");
+
         // This is the only way I've found to position the cursor in the element
-        var range = this.editor.createRange();
-        range.moveToElementEditablePosition( this.editor.editable(), true );
-        this.editor.getSelection().selectRanges( [ range ] );
-        
-        assert.areSame( CKEDITOR.TRISTATE_DISABLED, testCommand.state, 'autoNumberHeading DISABLEd in P' );
+      var range = this.editor.createRange();
+      range.moveToElementEditablePosition(this.editor.editable(), true);
+      this.editor.getSelection().selectRanges([ range ]);
+
+      assert.areSame(
+        CKEDITOR.TRISTATE_DISABLED, testCommand.state,
+        "autoNumberHeading DISABLEd in P"
+      );
 
     }
   });
