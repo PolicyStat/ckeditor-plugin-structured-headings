7ee0bd16bb0092306247cc6a17452eeea08210e1
BDeveau <brad.deveau@policystat.com>
2016-12-06 12:18:54 -0500

    First wave of tests
    * Command is active, but off inside non-numbered header
    * Command is active, and on inside numbered header
    * Command is disabled in a paragraph

--- tests/smoke/ckeditor.js
+++ tests/smoke/ckeditor.js
@@ -1,27 +1,64 @@
-/* bender-tags: editor */
+/* bender-tags: editor,unit */
 /* bender-ckeditor-plugins: wysiwygarea,structuredheadings,toolbar,basicstyles */
 
 // Clean up all instances been created on the page.
 (function () {
-  var removeAllInstances = function () {
-    var allInstances = CKEDITOR.instances;
-    for (var i in allInstances) {
-      CKEDITOR.remove(allInstances[i]);
-    }
-  };
 
+  bender.editor = {
+    allowedForTests: 'h1; p'
+  };
+  
   bender.test({
     setUp: function () {
-      removeAllInstances();
+      //Anything to be run before each test if needed
     },
 
     "CKEditor Loads": function () {
-      CKEDITOR.replace("editor1");
+      CKEDITOR.replace('editor1')
       assert.isObject(CKEDITOR.instances.editor1, "editor instance not found");
     },
 
     "Structured Headings Plugin Available": function () {
       assert.isObject(CKEDITOR.plugins.get("structuredheadings"), "plugin not available");
+    },
+    
+    'Command Active but Off Inside Header Tag': function() {
+        this.editorBot.setHtmlWithSelection('<h1>Heading</h1>');
+        var testCommand = this.editor.getCommand( 'autoNumberHeading' );
+        
+        // This is the only way I've found to position the cursor in the element
+        var range = this.editor.createRange();
+		range.moveToElementEditablePosition( this.editor.editable(), true );
+		this.editor.getSelection().selectRanges( [ range ] );
+        
+        assert.areSame( CKEDITOR.TRISTATE_OFF, testCommand.state, 'autoNumberHeading OFF in H1' );
+
+    },
+    
+    'Command Active and On Inside AutoNumbered Header': function() {
+        this.editorBot.setHtmlWithSelection('<h1 class="autonumber">Heading</h1>');
+        var testCommand = this.editor.getCommand( 'autoNumberHeading' );
+        
+        // This is the only way I've found to position the cursor in the element
+        var range = this.editor.createRange();
+        range.moveToElementEditablePosition( this.editor.editable(), true );
+        this.editor.getSelection().selectRanges( [ range ] );
+        
+        assert.areSame( CKEDITOR.TRISTATE_ON, testCommand.state, 'autoNumberHeading ON in H1' );
+
+    },
+    
+    'Command Disabled Outside Header': function() {
+        this.editorBot.setHtmlWithSelection('<p> Paragraph </p>');
+        var testCommand = this.editor.getCommand( 'autoNumberHeading' );
+        
+        // This is the only way I've found to position the cursor in the element
+        var range = this.editor.createRange();
+        range.moveToElementEditablePosition( this.editor.editable(), true );
+        this.editor.getSelection().selectRanges( [ range ] );
+        
+        assert.areSame( CKEDITOR.TRISTATE_DISABLED, testCommand.state, 'autoNumberHeading DISABLEd in P' );
+
     }
   });
 })();
