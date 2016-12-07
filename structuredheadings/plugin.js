dc3a83460d2e2f73831960707230a1ebe2f52fbf
BDeveau <brad.deveau@policystat.com>
2016-12-07 16:09:09 -0500

    Initial Command of ToggleHeading
    Some refactor changes to helper functions to accomodate a new command
    Some changes to icons to differentiate each command
    (even though the icon doesn't work for toggleHeading)
    
    ToggleHeading functionality completed but needs review tomorrow

--- structuredheadings/plugin.js
+++ structuredheadings/plugin.js
@@ -1,6 +1,6 @@
 (function () {
   CKEDITOR.plugins.add("structuredheadings", {
-    icons: "structuredheadings",
+    icons: "autonumberheading, toggleheading",
     init: function (editor) {
       // list of elements allowed to be numbered
       var allowedElements = ["h1", "h2", "h3", "h4", "h5", "h6"];
@@ -8,60 +8,117 @@
       editor.addContentsCss(this.path + "styles/numbering.css");
 
       // Helper function to set button state
-      var setCommandState = function (state) {
+      var setCommandState = function (command,state) {
         switch (state) {
         case "on":
-          editor.getCommand("autoNumberHeading").setState(CKEDITOR.TRISTATE_ON);
+          editor.getCommand(command).setState(CKEDITOR.TRISTATE_ON);
           break;
         case "off":
-          editor.getCommand("autoNumberHeading").setState(CKEDITOR.TRISTATE_OFF);
+          editor.getCommand(command).setState(CKEDITOR.TRISTATE_OFF);
           break;
         case "disabled":
-          editor.getCommand("autoNumberHeading").setState(CKEDITOR.TRISTATE_DISABLED);
+          editor.getCommand(command).setState(CKEDITOR.TRISTATE_DISABLED);
           break;
         }
 
+      };
+      
+      var isNumbered = function (element) {
+        if (element.hasClass("autonumber")) {
+          return true;
+        } else {
+          return false;
+	}
       };
 
       // The command to add an autonumbering class to selection
       editor.addCommand("autoNumberHeading", {
         allowedContent: "h1(*); h2(*); h3(*); h4(*); h5(*); h6(*)",
-        requiredContent: allowedElements.join(";"),
         startDisabled: true,
         exec: function () {
           var element = editor.getSelection().getStartElement();
 
-          if (!element.hasClass("autonumber")) {
+          if (!isNumbered(element)) {
             element.addClass("autonumber");
-            setCommandState("on");
+            setCommandState("autoNumberHeading","on");
           } else {
             element.removeClass("autonumber");
-            setCommandState("off");
+            setCommandState("autoNumberHeading","off");
           }
         }
+      });
+      
+      editor.addCommand("toggleHeading", {
+	  startDisabled: true,
+	  exec: function () {
+	    var element = editor.getSelection().getStartElement();
+	    //find previous element that matches allowedElements
+	    var previousHeader = element.getPrevious( function (node) {
+		if(allowedElements.indexOf(node.getName()) >= 0) {
+		    return true;
+		}
+	    });
+	    
+	    //Get previous element style (type) and apply to current selection, otherwise new H1
+	    if(previousHeader) {
+              var style = new CKEDITOR.style( { element: previousHeader.getName()});
+	      // if previous was numbered, set the new  one to numbered also
+	      if(isNumbered(previousHeader)) {
+		  style = new CKEDITOR.style( { element: previousHeader.getName(),
+		      				attributes: {'class': 'autonumber'}});
+	      }
+	      editor.applyStyle( style );
+	    } else {
+	      var style = new CKEDITOR.style( { element: "h1" } );
+	      editor.applyStyle( style );
+	    }
+	    
+	  }
+	  
       });
 
       // Add the button to the toolbar only if toolbar plugin or button plugin is loaded
       if (!!CKEDITOR.plugins.get("toolbar") || !!CKEDITOR.plugins.get("button")) {
-        editor.ui.addButton("structuredheadings", {
+        editor.ui.addButton("autonumberheading", {
           label: "Autonumber Heading",
           command: "autoNumberHeading",
           toolbar: "styles,0"
         });
+        editor.ui.addButton("toggleheading", {
+            label: "Toggle Heading",
+            command: "toggleHeading",
+            toolbar: "styles,1"
+          });
       }
 
-      // Set button state on selection change.
-      // On/off for element style, disabled for invalid element
+      /* Set button state on selection change.
+      * On/off for element style, disabled for invalid element
+      */
       editor.on("selectionChange", function (e) {
         var element = e.data.selection.getStartElement();
+        
         if (allowedElements.indexOf(element.getName()) >= 0) {
-          if (element.hasClass("autonumber")) {
-            setCommandState("on");
+          //if is autonumbered, update state appropriately
+          if (isNumbered(element)) {
+            setCommandState("autoNumberHeading","on");
           } else {
-            setCommandState("off");
+            setCommandState("autoNumberHeading","off");
           }
+          
+          // if it's any header, turn set toggle heading on
+          setCommandState("toggleHeading","on");
+        
+        /* special case for p tags, for toggleHeading command, could be handled better
+        * this is to allow toggleHeading active on p tags without including it in
+        * our allowedElements for autonumbering
+        */
+        } else if (element.getName() === "p") {
+            setCommandState("toggleHeading", "off");
+            setCommandState("autoNumberHeading","disabled");
+        //disable otherwise
         } else {
-          setCommandState("disabled");
+          setCommandState("autoNumberHeading","disabled");
+          setCommandState("toggleHeading","disabled");
         }
 
       });
