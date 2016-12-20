12f7792cb33898575d3b3505b786bdb7b7d2ff92
BDeveau <brad.deveau@policystat.com>
2016-12-20 16:23:12 -0500

    Dialog is now HTML (whew..)

--- structuredheadings/dialogs/selectstyle.js
+++ structuredheadings/dialogs/selectstyle.js
@@ -1,12 +1,59 @@
 CKEDITOR.dialog.add("selectStyle", function (editor) {
 
-  var styleOptions = [];
+  var columns = 4;
+  var htmlContent = "";
+  htmlContent += "<div>";
+  htmlContent += "<table role=\"listbox\"";
+  htmlContent += "style=\"width:100%;height:100%;border-collapse:separate;\"";
+  htmlContent += "cellspacing=\"2\" cellpadding=\"2\"";
+  if (CKEDITOR.env.ie && CKEDITOR.env.quirks) {
+    htmlContent += "style=\"position:absolute;\"";
+  }
+  htmlContent += "><tbody>";
 
-  //Build out the style options
+  //build out HTML
   (function () {
+    var i = 0;
+
     for (var style in editor.config.autonumberStyles) {
-      styleOptions.push([style, style]);
+
+          //If we're at column limit, start new row
+      if (i % columns === 0) {
+        htmlContent += "<tr>";
+      }
+
+      //Setup for each style item
+      var styleOption = [];
+      styleOption += "<input type=\"radio\" name=\"styleChooser\" style=\"text-align: center;\"" +
+      "id=\" style_label_" + i + "_" + CKEDITOR.tools.getNextNumber() + "\"" +
+      "value=\"" + style + "\">" + style +
+      "</input>";
+
+      htmlContent += "<td class=\"cke_dark_background cke_centered\"" +
+      "style=\"vertical-align: middle;\">" +
+      styleOption +
+      "</td>";
+
+      //end the new row
+      if (i % columns === columns - 1) {
+        htmlContent += "</tr>";
+      }
+
+
+      //increment counter
+      i++;
     }
+
+    //padding for if last row is short
+    if (i < columns - 1) {
+      for (; i < columns - 1; i++) {
+        htmlContent += "<td></td>";
+      }
+      htmlContent += "</tr>";
+    }
+
+    htmlContent += "</tbody></table></div>";
+
   })();
 
   return {
@@ -16,24 +63,28 @@ CKEDITOR.dialog.add("selectStyle", function (editor) {
     contents: [
       {
         id: "tab-style",
-        label: "Style Options",
+        label: "test",
+        title: "test tab",
         elements: [
           {
-            type: "radio",
-            id: "styleChooser",
-            items: styleOptions
+            type: "html",
+            html: htmlContent
           }
-        ]
+        ],
+        style: "width: 100%; border-collapse: separate;"
       }
+
     ],
     onOk: function () {
-      var style = this.getValueOf("tab-style", "styleChooser");
+      var style = this.getElement().getDocument().findOne("input:checked").getValue();
       if (editor.execCommand("setCurrentStyle", style)) {
         editor.execCommand("reapplyStyle");
       }
     },
     onShow: function () {
       var document = this.getElement().getDocument();
+
+      //Set current style input as active
       var elementList = document.find("input[value='" +
               editor.config.autonumberCurrentStyle + "']");
       if (elementList.count() > 0) {
