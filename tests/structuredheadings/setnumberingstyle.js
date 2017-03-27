88930421ce76235494563a4bc4e6b219b3d39af0
Alex Tsai <caffodian@gmail.com>
2017-03-27 13:56:59 -0700

    initial click the 1aiai test

--- tests/structuredheadings/setnumberingstyle.js
+++ tests/structuredheadings/setnumberingstyle.js
@@ -12,4 +12,23 @@
     setUp: function () {
       //Anything to be run before each test if needed
     },
+    "apply numbering style to a non-autonumbered heading": function () {
+      var bot = this.editorBot;
+      var ed = this.editor;
+      bot.setHtmlWithSelection("<h1>^foo</h1>");
+      var name = "NumStyles";
+      var styleCombo = ed.ui.get(name);
+      assert.areSame(CKEDITOR.TRISTATE_OFF, styleCombo._.state, "check state OFF");
+
+      bot.combo(name, function (combo) {
+        assert.areSame(CKEDITOR.TRISTATE_ON, combo._.state, "check state ON when opened");
+        combo.onClick("1. a. i. a. i.");
+        assert.areSame(
+            "<h1 class=\"autonumber autonumber-0 autonumber-N\">^foo</h1>",
+            bot.htmlWithSelection(),
+            "applied 1aiai to h1"
+        );
+      });
+    }
+  });
 })();
