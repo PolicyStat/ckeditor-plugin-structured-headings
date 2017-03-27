4dc3dbbcf36416f5ea963c97145520cc2ef30737
Alex Tsai <caffodian@gmail.com>
2017-03-27 11:38:28 -0700

    so this combo doesn't seem to have coverage either

---
+++ tests/structuredheadings/setnumberingstyle.js
@@ -0,0 +1,15 @@
+/* bender-tags: editor,unit */
+/* bender-ckeditor-plugins: wysiwygarea,structuredheadings,toolbar,basicstyles,dialog,richcombo */
+
+// Clean up all instances been created on the page.
+(function () {
+
+  bender.editor = {
+    allowedForTests: "h1; h2; p"
+  };
+
+  bender.test({
+    setUp: function () {
+      //Anything to be run before each test if needed
+    },
+})();
