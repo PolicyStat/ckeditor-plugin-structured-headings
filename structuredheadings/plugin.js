30ae543432ad611bbb55d67d443c349176186a72
Alex Tsai <caffodian@gmail.com>
2017-04-05 08:00:05 -0700

    name the walker evaluator

--- structuredheadings/plugin.js
+++ structuredheadings/plugin.js
@@ -451,7 +451,7 @@
     },
     clearAllFromRange: function (editor, range) {
       var walker = new CKEDITOR.dom.walker(range); // eslint-disable-line new-cap
-      walker.evaluator = function (node) {
+      walker.evaluator = function isParaPreOrHeading(node) {
         if (node.type !== CKEDITOR.NODE_ELEMENT) {
           return false;
         }
