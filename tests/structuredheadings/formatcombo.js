b593d327c3a082aa1f4e087950c7643bde63ba86
Alex Tsai <caffodian@gmail.com>
2017-05-16 11:18:06 -0700

    a mixed test case

--- tests/structuredheadings/formatcombo.js
+++ tests/structuredheadings/formatcombo.js
@@ -54,6 +54,24 @@
         );
       });
     },
+
+    "mix range of ps and headings all become autonumbered": function () {
+      var bot = this.editorBot;
+      bot.setHtmlWithSelection("[<p>foo</p><h1>bar</h1><p>baz]</p>");
+
+      bot.combo(comboName, function (combo) {
+        // click h2
+        combo.onClick("h2");
+        // the new h2 has autonumbering set, since no prior heading exists
+        assert.areSame(
+            "<h2 class=\"autonumber autonumber-1\">foo</h2>" +
+            "<h2 class=\"autonumber autonumber-1\">bar</h2>" +
+            "<h2 class=\"autonumber autonumber-1\">baz</h2>",
+            bot.getData(),
+            "applied h2 block autonumberstyle"
+        );
+      });
+    },
     // eslint-disable-next-line max-len
     "when creating next level heading, the autonumbering and numbering scheme css of the next level is used": function () {
       var bot = this.editorBot;
