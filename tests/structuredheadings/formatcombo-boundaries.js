1aaec7adc48d74ee43f757190b128cb3fc9c77e5
Alex Tsai <caffodian@gmail.com>
2017-05-29 15:36:28 -0700

    new tests file as I investigate this tripleclick craziness

---
+++ tests/structuredheadings/formatcombo-boundaries.js
@@ -0,0 +1,41 @@
+/* eslint-disable */
+/* bender-tags: editor,unit */
+/* bender-ckeditor-plugins: wysiwygarea,structuredheadings,toolbar,basicstyles,dialog,richcombo,undo */
+/* eslint-enable */
+
+// Clean up all instances been created on the page.
+(function () {
+  var comboName = "NumFormats";
+
+  bender.editor = {
+    allowedForTests: "h1; h2; p"
+  };
+
+  bender.test({
+    setUp: function () {
+      //Anything to be run before each test if needed
+    },
+
+    tearDown: function () {
+      var editor = this.editorBot.editor;
+      editor.plugins.structuredheadings.currentScheme = "1. a. i. a. i.";
+    },
+
+    "applies headings correctly when paragraph is followed by another paragraph, and triple clicked": function () {
+      var bot = this.editorBot;
+      bot.setHtmlWithSelection("<p>[foo</p><p>]bar</p>");
+
+      bot.combo(comboName, function (combo) {
+        // click h2
+        combo.onClick("h2");
+        // the new h2 has autonumbering set, since no prior heading exists
+        assert.areSame(
+          "<h2 class=\"autonumber autonumber-a autonumber-1\">foo</h2>" +
+          "<h2 class=\"autonumber autonumber-a autonumber-1\">bar</h2>",
+          bot.getData(),
+          "applied h2 and remains autonumbered"
+        );
+      });
+    }
+  });
+})();
