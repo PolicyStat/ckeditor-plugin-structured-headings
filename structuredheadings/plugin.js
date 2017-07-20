1ceac349c704d11e263451ea90dc10ab038a0be1
Alex Tsai <caffodian@gmail.com>
2017-07-20 11:59:11 -0700

    Revert "Revert "wtf, I don't know why bender won't pick this change up" because i'm paranoid about the local bender failure"
    This reverts commit 6583dc31e73b088cedb75d175f7edfad779ad5f4.

--- structuredheadings/plugin.js
+++ structuredheadings/plugin.js
@@ -432,7 +432,8 @@
         label: "Numbering",
         title: "Numbering",
         toolbar: "styles,8",
-        allowedContent: "h1(*); h2(*); h3(*); h4(*); h5(*); h6(*)",
+        allowedContent: "h1(*); h2(*); h3(*); h4(*); h5(*); h6(*);" +
+          "ol(list-upper-alpha,list-lower-alpha,list-decimal,list-upper-roman,list-lower-roman)",
 
         panel: {
           css: [ CKEDITOR.skin.getPath("editor") ].concat(editor.config.contentsCss),
