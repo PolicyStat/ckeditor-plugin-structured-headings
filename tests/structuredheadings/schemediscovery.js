13e16b3854b27957a478a7725e913447d08d2f5d
Alex Tsai <caffodian@gmail.com>
2017-12-06 12:28:26 -0500

    refs #88 - side effect from fix, when people feed bad CSS, it defaults to no numbering in future headings.  This is probably okay

--- tests/structuredheadings/schemediscovery.js
+++ tests/structuredheadings/schemediscovery.js
@@ -32,7 +32,7 @@
       editor.plugins.structuredheadings.currentScheme = "1.1.1.1.1.";
     },
 
-    "default is numbered": function () {
+    "default is not numbered": function () {
       var bot = this.editorBot;
       var editor = bot.editor;
       bot.setHtmlWithSelection(
@@ -40,8 +40,10 @@
         "<h2 class=\"autonumber autonumber-1\">bar</h2>"
       );
 
+      // because this was mangled CSS, do non-numbered until otherwise
+      // explicitly chosen
       assert.areEqual(
-        "1.1.1.1.1.",
+        null,
         detectScheme(editor)
       );
     },
